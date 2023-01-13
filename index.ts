6203162918d01c3c5a509e27b609ca6d16e260b5
Tom <admin@Admins-MacBook-Pro.local>
2023-01-13 00:06:56 +0000

    Add basics for placing Rover and direction type

--- index.ts
+++ index.ts
@@ -1,5 +1,16 @@
 import { clear, print, askQuestion } from './console';
 
+const directions = ["N", "S", "E", "W"] as const;
+
+const directionsObj = {
+    "N": "North",
+    "S": "South",
+    "E": "East",
+    "W": "West",
+}
+
+type Direction = typeof directions[number];
+
 export function startRover(): void {
 	clear();
 	askQuestion(`Enter Plataeu Size (x y e.g. 5 5)`, createGrid); 
@@ -30,7 +41,31 @@ function createGrid(size: string): number[][] {
 
     console.log(fullMatrix);
 
+
+	askQuestion('Enter Rovers starting position', placeRover);
+
     return fullMatrix;
+}
+
+function placeRover(position: string): boolean {
+    // 1 2 N
+    // x: number, y: number, direction: Direction
+    // Don’t allow numbers larger than the size of the grid, or any letters outside of the Direction type.
+
+    let splitString = position.split(' ');
+
+    // check >= grid x 
+    let x = splitString[0]
+
+    // check >= grid y 
+    let y = splitString[1];
+
+    // TODO check type is Direction
+    let direction = splitString[2];
+
+    print(`Rover placed at x=${x}, y=${y}, facing ${direction}`)
+    
+    return true;
 }
 
 startRover();
